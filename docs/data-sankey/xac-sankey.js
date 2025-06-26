25afc7eaf4ddb09029fd83e345a3d63be9cde4e5
DerekFurstPitt <drf57@pitt.edu>
2025-06-26 15:35:54 -0400

    replaced implementation of data-sankey with the updated one from sennet

--- docs/data-sankey/xac-sankey.js
+++ docs/data-sankey/xac-sankey.js
@@ -4,18 +4,13 @@ const initSankey = async () => {
             useShadow: true,
             isProd: true,
             styleSheetPath: '/data-sankey/xac-sankey.css',
-            validFilterMap: {
-                dataset_type: 'dataset_type_hierarchy',
-                status: null,
-                source_type: 'dataset_source_type',
-            },
             groupByOrganCategoryKey: 'term',
             api: {
                 sankey: 'https://entity-api.dev.hubmapconsortium.org/datasets/sankey_data',
                 context: 'hubmap'
             }
         })
-    )
+    );
     const el = document.getElementById('js-sankey')
     el.innerHTML = `<consortia-sankey options="${sankeyOptions}" />`
 
@@ -24,28 +19,25 @@ const initSankey = async () => {
         if (ctx.setOptions) {
             let adapter = new HuBMAPAdapter(ctx, {isProd: true})
             clearInterval(i)
-            ctx.setOptions({
-                onDataBuildCallback: () => adapter.onDataBuildCallback(),
-                onNodeBuildCssCallback: (d) => {
-                    if (d.columnName === ctx.validFilterMap.dataset_type) {
-                        const assay = adapter.captureByKeysValue({matchKey: d.columnName, matchValue: d.name, keepKey: 'dataset_type_description'}, ctx.rawData)
-                        return assay.length <= 0 ? 'c-sankey__node--default' : ''
-                    }
-                    return ''
-                },
-                onLinkClickCallback: (e, d) => {
-                    e.preventDefault()
-                    adapter.goToFromLink(d)
-                },
-                onNodeClickCallback: (e, d) => {
-                    e.preventDefault()
-                    adapter.goToFromNode(d)
-                },
-                onLabelClickCallback: (e, d) => {
-                    e.preventDefault()
-                    adapter.goToFromNode(d)
+            ctx.setOptions(
+                {
+                    onDataBuildCallback: () => {
+                        adapter.onDataBuildCallback()
+                    },
+                    onLinkClickCallback: (e, d) => {
+                        e.preventDefault()
+                        adapter.goToFromLink(d)
+                    },
+                    onNodeClickCallback: (e, d) => {
+                        e.preventDefault()
+                        adapter.goToFromNode(d)
+                    },
+                    onLabelClickCallback: (e, d) => {
+                        e.preventDefault()
+                        adapter.goToFromNode(d)
+                    },
                 }
-            })
+            )
         }
     }, 500)
 }
